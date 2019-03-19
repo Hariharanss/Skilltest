f1cbdfbe7b1862fb9e7f102ad25ee7b49d0a9dbc
Hariharanss <hariwebhost01@gmail.com>
2019-03-19 11:54:15 +0530

    U

--- src/config/dbconfig.js
+++ src/config/dbconfig.js
@@ -1,16 +1,35 @@
 var sql = require('mssql');
 
-var config = {
+var db_config = {
     user: 'sa',
     password: 'orbitz',
-    server: 'orbits-9', 
+    server: 'orbits-9',
     database: 'OnlineTesting'
 };
 
-sql.connect(config, function (err) {
+function handleDisconnect() {
 
-    if (err) console.log(err);
+    sql.connect(db_config, function (err) {
 
-});
+        if(err) {                         
+            console.log('error when connecting to db:', err);
+            sql.close();
+            setTimeout(handleDisconnect, 5 * 60 * 1000);
+          }  
+
+    });
+
+
+  sql.on('error', function(err) {
+    console.log('------------------db error------------------------', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
+      handleDisconnect();                         
+    } else {                                      
+      throw err;                                  
+    }
+  });
+}
+
+handleDisconnect();
 
 module.exports=sql;
